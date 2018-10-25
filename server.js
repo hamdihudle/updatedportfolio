e54d87ab8c2b22e3ef4c6f484993432518e3a14e
Hamdi Hudle <hamdihudle@Hamdis-MacBook-Air.local>
2018-10-25 14:42:44 -0500

    heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+// set port 
+
+var port = process.env.PORT || 8080 
+
+app.use(express.static(__dirname));
+
+app.get("/",function(req, req){
+    resizeBy.render("index")
+})
+
+app.listen(port, function() {
+    console.log("app running");
+})
